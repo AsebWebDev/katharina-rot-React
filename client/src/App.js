af2e4ddd96ec5a997fa544b945713259816b1ba6
André Sebastian <a.sebastian@posteo.de>
2019-04-19 18:52:12 +0200

    ad mdbreact, add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import api from '../api';
 import './App.css';
 
 class App extends Component {
+  
+  handleLogoutClick(e) {
+    api.logout()
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Katharina Rot</h1>
+          <NavLink to="/" exact>Home</NavLink>
+          <NavLink to="/admin">Admin</NavLink>
+          {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
+          {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
+          <NavLink to="/secret">Secret</NavLink>
         </header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/admin" component={Admin} />
+          <Route render={() => <h2>404</h2>} />
+        </Switch>
       </div>
     );
   }
