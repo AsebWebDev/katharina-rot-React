c71306b380dd0f9fc25f06f790733749b1b8843f
André Sebastian <a.sebastian@posteo.de>
2019-10-05 18:03:46 +0200

    clean code, fix: async uploads lead to only one picture stored in bulk upload

--- client/src/components/pages/Admin.jsx
+++ client/src/components/pages/Admin.jsx
@@ -5,62 +5,53 @@ import {newNotification} from '../../actioncreators'
 import '../../configs/cloudinary'
 import api from '../../api';
 
-
-
 function Admin (props){
   let {dispatch} = props;
-  let [title, setTitle] = useState('');
+  let [currentCollection, setCurrentCollection] = useState({})
   let [titlePic, setTitlePic] = useState('');
   let [pictures, setPictures] = useState([]);
   let [tags, setTags] = useState('');
-  let [description, setDescription] = useState('');
 
   let uploadWidget = (e) => {
-    let multiple = (e.target.id === "upload-art"); // Multiple pictures for the gallery or one picture as title
     e.preventDefault();
+    let uploadedPictures = []
+    let multiple = (e.target.id === "upload-art"); // Multiple pictures for the gallery or one picture as title
     window.cloudinary.createUploadWidget({ 
       upload_preset: 'mu7bkqlz',
       multiple
     },(error, result) => {
-        if (!error && result && result.event === "success") { 
+        if (!error && result && result.event === "success") { // If one file is uploaded, store in array to be pushed after user closes widget
+          uploadedPictures.push(result.info.secure_url)
+        }
+        if (!error && result && result.event === "close") { // If user closes widget use all uploaded pictures stored while uploading
           (multiple) 
-          ? setPictures([...pictures, result.info.secure_url] ) // UPLOAD MULTIPLE PICTURES FOR THE GALLERY
-          : setTitlePic(result.info.secure_url) // UPLOAD SINGLE PICTURE FOR THE TITLE
-        } else if (error) console.log(error)
+          ? setPictures(uploadedPictures) // UPLOAD MULTIPLE PICTURES FOR THE GALLERY 
+          : setTitlePic(result.info.secure_url) // UPLOAD SINGLE PICTURE FOR THE TITLE  
+        }
     }).open(); 
   }
   
   let handleChange = (e) => {
-    let {value, name} = e.target;
-    switch (name) {
-      case "title": setTitle(value); break;
-      case "tags": setTags(value); break;
-      case "description": setDescription(value); break;
-      default: console.log("No target name provided")
-    }
+    setCurrentCollection({
+      ...currentCollection,
+      [e.target.id]: e.target.value
+    })
   }
 
   let handleSubmit = (e) => {
     e.preventDefault();
-    let data = {
-      title,
-      titlePic,
-      pictures,
-      tags: tags.split(' '),
-      description
-    };
+    let data = {...currentCollection, pictures, titlePic}
+    //FIXME: turn tags from string to array on the backend
     api.addCollection(data)
     .then(result => {
       console.log('SUCCESS!')
       dispatch(newNotification(`Your Collection '${result.Collection.title}' has been created`))
-      setTitle('')
-      setTitlePic('')
+      setCurrentCollection({})
       setPictures([])
-      setTags([])
-      setDescription('')
+      // setTags([])
     }).catch(err => dispatch(newNotification(err.toString())));
   }
-
+  
     if (api.isLoggedIn()) {
       return (
       <div>
@@ -70,11 +61,11 @@ function Admin (props){
         {titlePic && <img src={titlePic} alt="" width="70px"/>}
         <form onSubmit={handleSubmit}>
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" value={title} onChange={handleChange}/><br />
+          <input type="text" name="title" id="title" value={currentCollection.title || ''} onChange={handleChange}/><br />
           <label htmlFor="tags">Tags</label>
-          <input type="text" name="tags" id="tags" value={tags} onChange={handleChange}/><br />
+          <input type="text" name="tags" id="tags" value={currentCollection.tags || ''} onChange={handleChange}/><br />
           <label htmlFor="tags">Description</label>
-          <input type="textarea" name="description" id="description" value={description} onChange={handleChange}/><br />
+          <input type="textarea" name="description" id="description" value={currentCollection.description || ''} onChange={handleChange}/><br />
           <button onClick={uploadWidget} id="upload-art" className="cloudinary-button">Upload Art</button>
           <button type="submit" className="cloudinary-button">Submit</button>
         </form>
