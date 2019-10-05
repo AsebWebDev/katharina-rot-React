e0d61aac80d14992f153423654db62b128411b58
André Sebastian <a.sebastian@posteo.de>
2019-10-05 11:51:58 +0200

    add isAdmin middleware and apply to protected routes

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Collection = require('../models/Collection')
-
+const {isLoggedIn, isAdmin} = require('../middlewares')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -13,7 +13,8 @@ router.get('/', (req, res, next) => {
     .catch(err => next(err))
 });
 
-router.post('/:id/delete', (req, res, next) => {
+//TODO: protect route (is admin?)
+router.post('/:id/delete', isAdmin, (req, res, next) => {
   console.log("Hit Delete-ID: "+ req.params.id)
   Collection.deleteOne( { _id : req.params.id } )
   .then(result => {
@@ -26,7 +27,26 @@ router.post('/:id/delete', (req, res, next) => {
 });
 
 //TODO: protect route (is admin?)
-router.post('/', (req, res, next) => {
+router.put('/:id/edit', isAdmin, (req, res, next) => {
+  console.log("Hit Edit-ID: "+ req.params.id)
+  Collection.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    titlePic: req.body.titlePic,
+    pictures: req.body.pictures,
+    tags: req.body.tags,
+    description: req.body.description,
+  }, { new: true }) // To access the updated collection (and not the old collection)
+    .then(collection => {
+      res.json({
+        message: "Your collection has been updated",
+        collection
+      })
+    })
+    .catch(err => next(err))
+});
+
+//TODO: protect route (is admin?)
+router.post('/', isAdmin, (req, res, next) => {
   let { title, titlePic, pictures, tags, description } = req.body
   Collection.create({ title, titlePic, pictures, tags, description })
     .then(Collection => {
@@ -37,7 +57,5 @@ router.post('/', (req, res, next) => {
     })
     .catch(err => next(err))
 });
-
-
 
 module.exports = router;
