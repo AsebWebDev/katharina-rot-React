6864e5ad8fd5dd4f0473188a11888a41d5e29bdb
André Sebastian <a.sebastian@posteo.de>
2019-10-07 20:09:00 +0200

    turn class into function + hooks

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import api from '../../api';
 import Card from '../Card'
@@ -6,29 +6,27 @@ import EditModal from '../pages/EditModal'
 import './Home.css'
 import greenBanner from '../../media/banner-greenfuture-1-1024x287.jpg'
 
-class Home extends Component {
+function Home (props) {
+  let { dispatch } = props;
 
-  componentDidMount(){
-    // GET ALL ART-DATA FROM BACKEND-DATABASE
+  useEffect(() => {
     api.getCollections()
-    .then(collections => this.props.dispatch({
+    .then(collections => dispatch({
       type: "GET_DATA", 
       collections
     })).catch (err => console.log(err))
-  }
+  }, [dispatch])
 
-  render() {      
-    return (
-      <div className="Home">
-        <img className="banner" src={greenBanner} alt="green banner"/>
-        <div className="gallery">
-          {this.props.collections && this.props.collections.map((collection, i) => 
-          <div key={collection._id}><Card collection={collection} dispatch={this.props.dispatch}/></div>)}
-        </div>
-        {this.props.modal.isOpen && this.props.modal.isEdit && <EditModal />}
+  return (
+    <div className="Home">
+      <img className="banner" src={greenBanner} alt="green banner"/>
+      <div className="gallery">
+        {props.collections && props.collections.map((collection, i) => 
+        <div key={collection._id}><Card collection={collection} dispatch={props.dispatch}/></div>)}
       </div>
-    );
-  }
+      {props.modal.isOpen && props.modal.isEdit && <EditModal />}
+    </div>
+  );
 }
 
 function mapStateToProps(reduxState){
