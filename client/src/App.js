bbb4db80d661e1c42c83cb6800d0f68500de0119
André Sebastian <a.sebastian@posteo.de>
2019-10-08 13:23:27 +0200

    turn back into class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Home from './components/pages/Home';
@@ -8,27 +8,37 @@ import Header from './components/Header';
 import Notification from './components/Notification'
 import './App.css';
 
-function App(props) {
-  
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/login" component={Login} />
-        <Route render={() => <h2>404</h2>} />
-      </Switch>
-      <div style={{
-              position: "fixed",
-              top: "10px",
-              right: "10px",
-              zIndex: 9999
-          }}>
-          {props.notifications && !!props.notifications.length && <Notification notifications={props.notifications}/>}
+// TODO: * Implement User-Settings
+
+class App extends Component {
+
+  componentDidMount(){
+  }
+
+  componentDidUpdate() {
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/login" component={Login} />
+          <Route render={() => <h2>404</h2>} />
+        </Switch>
+        <div style={{
+                position: "fixed",
+                top: "10px",
+                right: "10px",
+                zIndex: 9999
+            }}>
+            {this.props.notifications && !!this.props.notifications.length && <Notification notifications={this.props.notifications}/>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function mapStateToProps(reduxState){
