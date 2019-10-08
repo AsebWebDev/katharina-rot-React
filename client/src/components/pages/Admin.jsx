0dccbc4c46e10d61d9da6800e617df800f2476ee
André Sebastian <a.sebastian@posteo.de>
2019-10-08 22:42:09 +0200

    move create logic: admin to new create component

--- client/src/components/pages/Admin.jsx
+++ client/src/components/pages/Admin.jsx
@@ -1,83 +1,51 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-import {newNotification} from '../../actioncreators'
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Create from './Create'
 import '../../configs/cloudinary'
 import api from '../../api';
 
 function Admin (props){
-  let {dispatch} = props;
-  let [currentCollection, setCurrentCollection] = useState({})
-  let [titlePic, setTitlePic] = useState('');
-  let [pictures, setPictures] = useState([]);
+  let [mode, setMode] = useState("default")
 
-  let uploadWidget = (e) => {
-    e.preventDefault();
-    let uploadedPictures = []
-    let multiple = (e.target.id === "upload-art"); // Multiple pictures for the gallery or one picture as title
-    window.cloudinary.createUploadWidget({ 
-      upload_preset: 'mu7bkqlz',
-      multiple
-    },(error, result) => {
-        if (!error && result && result.event === "success") { 
-          (multiple) 
-          ? uploadedPictures.push(result.info.secure_url)   // If one of few files is uploaded, store in array to be pushed after user closes widget
-          : setTitlePic(result.info.secure_url)             // UPLOAD SINGLE PICTURE FOR THE TITLE ON UPLOAD FINISH
-        }
-        if (!error && result && result.event === "close" && multiple) {   // If user closes widget use all uploaded pictures stored while uploading
-          setPictures(uploadedPictures)                     // UPLOAD MULTIPLE PICTURES FOR THE GALLERY ON CLOSE
-        }
-    }).open(); 
-  }
-  
-  let handleChange = (e) => {
-    setCurrentCollection({
-      ...currentCollection,
-      [e.target.id]: e.target.value
-    })
-  }
-
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    let data = {...currentCollection, pictures, titlePic}
-    api.addCollection(data)
-    .then(result => {
-      dispatch(newNotification(`Your Collection '${result.Collection.title}' has been created`, 'Created'))
-      setCurrentCollection({})
-      setPictures([])
-    }).catch(err => dispatch(newNotification(err.toString())));
-  }
-  
-    if (api.isLoggedIn()) {
-      return (
-      <div>
-        <h1>Admin</h1>
-        <CloudinaryContext cloudName="djyjdargg">
-          <button onClick={uploadWidget} className="cloudinary-button">Add Title Image</button>
-        {titlePic && <img src={titlePic} alt="" width="70px"/>}
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" value={currentCollection.title || ''} onChange={handleChange}/><br />
-          <label htmlFor="tags">Tags</label>
-          <input type="text" name="tags" id="tags" value={currentCollection.tags || ''} onChange={handleChange}/><br />
-          <label htmlFor="tags">Description</label>
-          <input type="textarea" name="description" id="description" value={currentCollection.description || ''} onChange={handleChange}/><br />
-          <button onClick={uploadWidget} id="upload-art" className="cloudinary-button">Upload Art</button>
-          <button type="submit" className="cloudinary-button">Submit</button>
-        </form>
-        </CloudinaryContext>
-        {pictures && pictures.map((picture, i) => <img src={picture} key ={i} alt="" width="70px"/>)}
-      </div>
-    )} else {
-      props.history.push("/login") // Redirect to the login page
-      return null
+  let toggle = (choice) => {
+    switch (choice) {
+      case 'create': setMode('create'); break;
+      default: console.log("Default")
     }
+  }
+  
+  if (api.isLoggedIn()) {
+    return (
+    <div>
+      <MDBContainer className="mt-5 text-center">
+        <MDBRow>
+          <MDBCol>
+            <MDBJumbotron>
+              <h2 className="h1 display-3">Hello, Sarah!</h2>
+              <p className="lead">This is your admin page. Play around!</p>
+              <hr className="my-2" />
+              <p>Make a choice :)</p>
+              <p className="lead">
+                <MDBBtn color="primary" onClick={() => toggle('create')}>Create Collection</MDBBtn>
+              </p>
+            </MDBJumbotron>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      {(mode === "create") && <Create />}
+      
+    </div>
+  )} else {
+    props.history.push("/login") // Redirect to the login page
+    return null
+  }
 }
 
 function mapStateToProps(reduxState){
   return {
     collections: reduxState.collections,
-    notifications: reduxState.notifications
+    notifications: reduxState.notifications,
   }
 }
 
---
+++ client/src/components/pages/Create.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { CloudinaryContext } from 'cloudinary-react';
+import api from '../../api';
+import { setUploadedPics, newNotification } from '../../actioncreators'
+
+function Create(props) {
+    let {dispatch} = props;
+    let [titlePic, setTitlePic] = useState('');
+    let [pictures, setPictures] = useState([]);
+    let [currentCollection, setCurrentCollection] = useState({})
+
+
+    let uploadWidget = (e) => {
+        e.preventDefault();
+        let uploadedPictures = []
+        let multiple = (e.target.id === "upload-art"); // Multiple pictures for the gallery or one picture as title
+        window.cloudinary.createUploadWidget({ 
+            upload_preset: 'mu7bkqlz',
+            multiple
+        },(error, result) => {
+            if (!error && result && result.event === "success") { 
+                (multiple) 
+                ? uploadedPictures.push(result.info.secure_url)   // If one of few files is uploaded, store in array to be pushed after user closes widget
+                : setTitlePic(result.info.secure_url)             // UPLOAD SINGLE PICTURE FOR THE TITLE ON UPLOAD FINISH
+            }
+            if (!error && result && result.event === "close" && multiple) {   // If user closes widget use all uploaded pictures stored while uploading
+                setPictures(uploadedPictures)                     // UPLOAD MULTIPLE PICTURES FOR THE GALLERY ON CLOSE
+            }
+        }).open(); 
+    }
+
+    let handleChange = (e) => {
+        setCurrentCollection({
+        ...currentCollection,
+        [e.target.id]: e.target.value
+        })
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        let data = {...currentCollection, pictures, titlePic}
+        api.addCollection(data)
+        .then(result => {
+            dispatch(newNotification(`Your Collection '${result.Collection.title}' has been created`, 'Created'))
+            setCurrentCollection({})
+            setPictures([])
+        }).catch(err => dispatch(newNotification(err.toString())));
+    }
+
+    return (
+        <div>
+            <CloudinaryContext cloudName="djyjdargg">
+                <button onClick={uploadWidget} className="cloudinary-button">Add Title Image</button>
+                {titlePic && <img src={titlePic} alt="" width="70px"/>}
+                <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" id="title" value={currentCollection.title || ''} onChange={handleChange}/><br />
+                <label htmlFor="tags">Tags</label>
+                <input type="text" name="tags" id="tags" value={currentCollection.tags || ''} onChange={handleChange}/><br />
+                <label htmlFor="tags">Description</label>
+                <input type="textarea" name="description" id="description" value={currentCollection.description || ''} onChange={handleChange}/><br />
+                <button onClick={uploadWidget} id="upload-art" className="cloudinary-button">Upload Art</button>
+                <button type="submit" className="cloudinary-button">Submit</button>
+                </form>
+            </CloudinaryContext>
+            {pictures && pictures.map((picture, i) => <img src={picture} key ={i} alt="" width="70px"/>)}
+        </div>
+    )
+}
+
+function mapStateToProps(reduxState){
+    return {
+      collections: reduxState.collections,
+      notifications: reduxState.notifications,
+    }
+}
+  
+export default connect(mapStateToProps)(Create)
