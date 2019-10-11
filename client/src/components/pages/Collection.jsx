a36e20e60c42b7291877a8714c9f1b1e5b26c9ad
André Sebastian <a.sebastian@posteo.de>
2019-10-11 23:21:07 +0200

    parse picture arrays for slider

--- client/src/components/pages/Collection.jsx
+++ client/src/components/pages/Collection.jsx
@@ -1,26 +1,51 @@
 import React, {useState, useEffect} from 'react'
 import { connect } from 'react-redux';
-import SimplaxTest from '../SimplaxTest'
+// import SimplaxTest from '../SimplaxTest2'
+import Slider from './SlideTest/Slider'
 import api from '../../api';
 
 function Collection(props) {
   let [currentId] = useState(props.match.params.id)
   let [currentCollection, setCurrentCollection] = useState(null)
+  let [parsedPictures, setParsedPictures] = useState([])
   let [error, setError] = useState(null)
-  console.log(currentCollection)
 
   useEffect(() => {
     api.getOneCollection(currentId)
     .then(result => setCurrentCollection(result.collection))
     .catch (err => setError(err))
   }, [currentId])
+
+  useEffect(() => {
+   let parsedPictures = [];
+   if (currentCollection && currentCollection.pictures) parsedPictures = parseDataForSlider(currentCollection.pictures)
+   setParsedPictures(parsedPictures)
+  }, [setParsedPictures, currentCollection])
+
+  let parseDataForSlider = array => {
+    return array.map((img,i) => ({
+      index: i,
+      headline: '',
+      button: '',
+      src: img
+    })
+  )}
   
   if (currentCollection)
     return (
       <div>
+                {/* <div style={{ 
+            width: "20vw",
+            height: "100vh",
+            borderRight: "2px solid black",
+            position: "fixed",
+            // top: "30vh",
+            top: 0,
+            left: 10
+        }}/> */}
           <p>{currentCollection.title}</p>
-          <SimplaxTest currentId={currentId}/>
-          {/* //TODO: add Lighbox Button and Lightbox */}
+          {currentCollection && currentCollection.pictures.length > 0 && <Slider heading="Example Slider" slides={parsedPictures} />}
+          {/* <SimplaxTest currentId={currentId}/> */}
           {/* {currentCollection.pictures.length > 0 && currentCollection.pictures.map((img,i) => <img key={i} src={img} alt="art"/>)} */}
       </div>
     )
