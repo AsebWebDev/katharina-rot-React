9c33b69c2fa82830c20906f58b19fa9c89f79b87
André Sebastian <a.sebastian@posteo.de>
2019-10-13 01:05:36 +0200

    move parallax data to config file

--- client/src/components/CollectionParallaxLeft.jsx
+++ client/src/components/CollectionParallaxLeft.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import { connect } from 'react-redux';
 import Plx from "react-plx";
 import api from '../api';
+import { parallaxDataGalleryLeft } from '../configs/parallax'
 import '../styles/CollectionParallaxLeft.css'
   
 function SimplaxTest(props) {
@@ -25,105 +26,15 @@ function SimplaxTest(props) {
         {currentCollection && currentCollection.pictures.map(pic => {
             let style = { zIndex: count, position: "relative", height: "70vh" }
             count-- //set Index - 1 to let the next one overlap this instance
-            return (<Plx parallaxData={parallaxData} style={style}><img alt="art" src={pic} width="400vw"/></Plx>)
-        }
-          
-        )}
+            return (<Plx parallaxData={parallaxDataGalleryLeft} style={style}><img alt="art" src={pic} width="400vw"/></Plx>)
+        })}
       </div>
       <div id="plx-right" className="plx">
-        
+      
       </div>
     </div>
   );
 }
-
-
-
-const parallaxData = [
-  {
-    start: "self",
-    startOffset: "",
-    end: "self",
-    endOffset: "50vh",
-    easing: "easeInSine",
-    properties: [
-        {
-            startValue: -15,
-            endValue: 25,
-            property: "translateX",
-            unit: "vw"
-        },
-        {
-          startValue: 1,
-          endValue: 8,
-          property: "translateY",
-          unit: "vw"
-        },
-        {
-          startValue: 0.5,
-          endValue: 1,
-          property: "brightness",
-          unit: ""
-        },
-        {
-          startValue: 1,
-          endValue: 0,
-          property: "grayscale",
-          unit: ""
-        }
-      ]
-  },
-  {
-    start: "self",
-    startOffset: "50",
-    end: "self",
-    endOffset: "90vh",
-    easing: "easeInSine",
-    properties: [
-        {
-            startValue: 1,
-            endValue: 1.5,
-            property: "scale",
-            unit: "vw"
-        },
-
-      ]
-  },
-  {
-    start: "self",
-    startOffset: "90vh",
-    end: "self",
-    endOffset: "100vh",
-    easing: "easeInSine",
-    properties: [
-        {
-            startValue: 25,
-            endValue: -18,
-            property: "translateX",
-            unit: "vw"
-          },
-          {
-            startValue: 1.5,
-            endValue: 1,
-            property: "scale",
-            unit: "vw"
-          },
-          {
-            startValue: 1,
-            endValue: 0.5,
-            property: "brightness",
-            unit: ""
-          },
-          {
-            startValue: 0,
-            endValue: 1,
-            property: "grayscale",
-            unit: ""
-          }
-    ]
-  },
-  
-];
 
 function mapStateToProps(reduxState){
     return {
---
+++ client/src/configs/parallax.js
@@ -0,0 +1,84 @@
+export const parallaxDataGalleryLeft = [
+    {
+        start: "self",
+        startOffset: "",
+        end: "self",
+        endOffset: "50vh",
+        easing: "easeInSine",
+        properties: [
+            {
+                startValue: -15,
+                endValue: 25,
+                property: "translateX",
+                unit: "vw"
+            },
+            {
+            startValue: 1,
+            endValue: 8,
+            property: "translateY",
+            unit: "vw"
+            },
+            {
+            startValue: 0.5,
+            endValue: 1,
+            property: "brightness",
+            unit: ""
+            },
+            {
+            startValue: 1,
+            endValue: 0,
+            property: "grayscale",
+            unit: ""
+            }
+        ]
+    },
+    {
+        start: "self",
+        startOffset: "50",
+        end: "self",
+        endOffset: "90vh",
+        easing: "easeInSine",
+        properties: [
+            {
+                startValue: 1,
+                endValue: 1.5,
+                property: "scale",
+                unit: "vw"
+            },
+
+        ]
+    },
+    {
+        start: "self",
+        startOffset: "90vh",
+        end: "self",
+        endOffset: "100vh",
+        easing: "easeInSine",
+        properties: [
+            {
+                startValue: 25,
+                endValue: -18,
+                property: "translateX",
+                unit: "vw"
+            },
+            {
+                startValue: 1.5,
+                endValue: 1,
+                property: "scale",
+                unit: "vw"
+            },
+            {
+                startValue: 1,
+                endValue: 0.5,
+                property: "brightness",
+                unit: ""
+            },
+            {
+                startValue: 0,
+                endValue: 1,
+                property: "grayscale",
+                unit: ""
+            }
+        ]
+    }
+];
