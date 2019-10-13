f65b51c5bf69f2725acbf80a5b3557a446ed8ae0
André Sebastian <a.sebastian@posteo.de>
2019-10-13 23:21:49 +0200

    use className, not class

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const SpinnerPage = () => {
   return (
     <>
-      <div class="spinner-grow text-primary" role="status">
-        <span class="sr-only">Loading...</span>
+      <div className="spinner-grow text-primary" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
-      <div class="spinner-grow text-success" role="status">
-        <span class="sr-only">Loading...</span>
+      <div className="spinner-grow text-success" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
-      <div class="spinner-grow text-danger" role="status">
-        <span class="sr-only">Loading...</span>
+      <div className="spinner-grow text-danger" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
-      <div class="spinner-grow text-warning" role="status">
-        <span class="sr-only">Loading...</span>
+      <div className="spinner-grow text-warning" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
-      <div class="spinner-grow text-info" role="status">
-        <span class="sr-only">Loading...</span>
+      <div className="spinner-grow text-info" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
     </>
   );
