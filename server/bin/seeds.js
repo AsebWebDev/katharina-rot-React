351d1d38d880f0638e8ac162d27386e3b4d59e5f
André Sebastian <a.sebastian@posteo.de>
2019-10-13 23:22:24 +0200

    use bCrypt

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -1,31 +1,45 @@
 function seedDB() {
     console.log("Seed triggered")
-    
     const mongoose = require("mongoose");
     const Collection = require("../models/Collection");
+    const User = require("../models/User");
+    const bcrypt = require("bcrypt");
+    const bcryptRounds = 10;
 
     require('../configs/database')
 
-    let collections = []
+    let users = [
+        {
+          username: "admin",
+          password: bcrypt.hashSync(process.env.ADMIN_PASSWORD, bcrypt.genSaltSync(bcryptRounds)),
+          isAdmin: true
+        }
+    ]
+    
+    // let collections = []
 
-    for (let i = 0; i < 30; i++) {
-        collections.push({
-            title: "Title " +  i,
-            titlePic: "https://community.adobe.com/legacyfs/online/avatars/a754554_Capture.png",
-            pictures: ["https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200"],
-            tags: [],
-            description: "Description Number " + i 
-        })
-    }
+    // for (let i = 0; i < 30; i++) {
+    //     collections.push({
+    //         title: "Title " +  i,
+    //         titlePic: "https://community.adobe.com/legacyfs/online/avatars/a754554_Capture.png",
+    //         pictures: ["https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200", "https://picsum.photos/id/379/200/200"],
+    //         tags: [],
+    //         description: "Description Number " + i 
+    //     })
+    // }
 
-    Promise.all([Collection.deleteMany()])
-    .then (() => Collection.create(collections))
+    // Promise.all([Collection.deleteMany()])
+    // .then (() => Collection.create(collections))
     // .then (() => mongoose.disconnect())
-    .catch(err => { mongoose.disconnect(); throw err; });
+    // .catch(err => { mongoose.disconnect(); throw err; });
 
+    Promise.all([User.deleteMany()])
+    .then (() => User.create(users))
+    .then (() => mongoose.disconnect())
+    .catch(err => { mongoose.disconnect(); throw err; });
 }
 
-// seedDB();
+seedDB();
 
 module.exports = {
     seedDB
