8b4be13a98ac58271292b0d4b05609cd8ae0e8dc
André Sebastian <a.sebastian@posteo.de>
2019-10-19 18:50:10 +0200

    add post route for new news

--- server/routes/news.js
+++ server/routes/news.js
@@ -9,4 +9,21 @@ router.get('/', (req, res, next) => {
     .catch(err => next(err))
 });
 
+router.post('/', (req, res, next) => {
+    let { title, description, tags} = req.body
+    console.log("News POST route hit.")
+    console.log(req.body)
+    let titlePic;
+    if (req.body.titlePic) titlePic = req.body.titlePic 
+    News.create({ title, titlePic, tags, description })
+    .then(news => {
+      console.log(news)
+      res.json({
+        success: true,
+        news
+      });
+    })
+    .catch(err => next(err))
+});
+
 module.exports = router;
