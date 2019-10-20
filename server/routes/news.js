b55ebd46ceb3c4b032073883ad7a8f705d508555
André Sebastian <a.sebastian@posteo.de>
2019-10-20 22:39:40 +0200

    add thumbnail to saved news

--- server/routes/news.js
+++ server/routes/news.js
@@ -3,21 +3,17 @@ const router = express.Router();
 const News = require('../models/News')
 
 router.get('/', (req, res, next) => {
-    console.log("News route hit.")
     News.find()
     .then(news => { res.json(news);})
     .catch(err => next(err))
 });
 
 router.post('/', (req, res, next) => {
-    let { title, description, tags} = req.body
-    console.log("News POST route hit.")
-    console.log(req.body)
-    let titlePic;
+    let { title, thumbnail, description, tags} = req.body
+    let titlePic
     if (req.body.titlePic) titlePic = req.body.titlePic 
-    News.create({ title, titlePic, tags, description })
-    .then(news => {
-      console.log(news)
+    News.create({ title, titlePic, thumbnail, tags, description })
+    .then(news => {  
       res.json({
         success: true,
         news
