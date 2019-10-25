d9ea83d6a7ea3876b5e6bf4eb26419b8436af924
André Sebastian <a.sebastian@posteo.de>
2019-10-25 18:01:25 +0200

    add News Page

--- client/src/components/pages/News.jsx
+++ client/src/components/pages/News.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { MDBCard, MDBCardBody } from "mdbreact";
+import MaxiNews from '../MaxiNews'
+import '../../styles/News.scss'
 
 function News(props) {
     return (
-        <div>
-            News
+        <div id="news">
+            <MDBCard className="my-5 px-5 pb-5">
+                <MDBCardBody>
+                    <h2 className="h1-responsive font-weight-bold text-center my-5">
+                    Recent posts
+                    </h2>
+                    <p className="text-center w-responsive mx-auto mb-5">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                    </p>
+                    
+                    {props.news.map((news,i) => <MaxiNews news={news} i={i} key={i}/>)}
+
+                </MDBCardBody>
+            </MDBCard>
         </div>
     )
 }
@@ -15,4 +33,4 @@ function mapStateToProps(reduxState){
     }
   }
   
-  export default connect(mapStateToProps)(News)
+export default connect(mapStateToProps)(News)
