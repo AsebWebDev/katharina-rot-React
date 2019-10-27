e46e54ead546cdbe5ae0dcbce6f730d3383b4c71
André Sebastian <a.sebastian@posteo.de>
2019-10-27 20:35:58 +0100

    make description expandable

--- client/src/components/MaxiNews.jsx
+++ client/src/components/MaxiNews.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import {  MDBRow, MDBCol, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
+import React, {useState, useEffect} from 'react'
 import TimeAgo from 'react-timeago'
 import germanStrings from 'react-timeago/lib/language-strings/de'
 import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
@@ -8,38 +7,32 @@ import '../styles/MaxiNews.scss'
 const formatter = buildFormatter(germanStrings)
 
 export default function MaxiNews(props) {
-    const {news} = props;
+    const {news,i} = props;
+    let [expand, setExpand] = useState(false)
+    
+    const toggle = () => {
+        setExpand(!expand)
+    }
+
+    const section = "section" + i
+    const collapsible = section + " section collapsible"
+
     return (
-        <div className="maxi-news">
-            <MDBRow>
-                <MDBCol lg="5" xl="4">
-                    <MDBView hover className="rounded z-depth-1-half mb-lg-0 mb-4">
-                    <img
-                        className="img-fluid"
-                        src={news.titlePic}
-                        alt={news.title}
-                    />
-                    <a href="#!">
-                        <MDBMask overlay="white-slight" />
-                    </a>
-                    </MDBView>
-                </MDBCol>
-                <MDBCol lg="7" xl="8">
-                <h3 className="font-weight-bold mb-3 p-0">
-                    <strong>{news.title}</strong>
-                </h3>
-                <p className="dark-grey-text">
-                    {news.description}
-                </p>
-                <p>
-                    by <a href="#!" className="font-weight-bold">Katharina Rot</a>, <span>{<TimeAgo date={news.created_at} formatter={formatter} />} </span> 
-                </p>
-                <MDBBtn color="primary" size="md">
-                    Read More
-                </MDBBtn>
-                </MDBCol>
-            </MDBRow>
-            <hr className="my-5" />
+        <div className="maxi-news2" onClick={toggle}>
+            <div id="news-top">
+                <img src={news.titlePic} alt="Title" />
+                <div id="mini-data">
+                    <p>by <a href="#!" className="font-weight-bold">Katharina Rot</a>, <span>{<TimeAgo date={news.created_at} formatter={formatter} />} </span></p>
+                    <p id="heart">heart</p>
+                </div>
+            </div>
+            <div id="news-bottom">
+                <div id="title"><strong>{news.title}</strong></div>
+                <div id="description">
+                    {!expand &&<p className={collapsible}>{news.description.slice(0,300)}{news.description.length > 300 ? "..." : ""}</p>}
+                    {expand &&<p className={section}>{news.description}</p>}
+                </div>
+            </div>
         </div>
     )
 }
