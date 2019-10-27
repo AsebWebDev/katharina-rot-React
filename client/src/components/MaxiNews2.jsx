ce1621313127602fbc517e0cb5ab456732dc0616
André Sebastian <a.sebastian@posteo.de>
2019-10-27 17:17:10 +0100

    style 2. version of maxi news

--- client/src/components/MaxiNews2.jsx
+++ client/src/components/MaxiNews2.jsx
@@ -11,15 +11,20 @@ export default function MaxiNews(props) {
     const {news} = props;
     return (
         <div className="maxi-news2">
-            <img src={news.titlePic} alt="Title" />
-            <div id="mini-data">
-                <p>by <a href="#!" className="font-weight-bold">Katharina Rot</a>, <span>{<TimeAgo date={news.created_at} formatter={formatter} />} </span></p>
-                <p>heart</p>
+            <div id="news-top">
+                <img src={news.titlePic} alt="Title" />
+                <div id="mini-data">
+                    <p>by <a href="#!" className="font-weight-bold">Katharina Rot</a>, <span>{<TimeAgo date={news.created_at} formatter={formatter} />} </span></p>
+                    <p id="heart">heart</p>
+                </div>
             </div>
-            <div id="title"><strong>{news.title}</strong></div>
-            <div id="description">
-                <p>{news.description}</p>
+            <div id="news-bottom">
+                <div id="title"><strong>{news.title}</strong></div>
+                <div id="description">
+                    <p>{news.description.slice(0,300)}{news.description.length > 300 ? "..." : ""}</p>
+                </div>
             </div>
+
         </div>
     )
 }
