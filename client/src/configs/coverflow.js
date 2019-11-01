022179d5936da29b0d4cae95dabd761642f82ed6
André Sebastian <a.sebastian@posteo.de>
2019-11-01 19:33:46 +0100

    move coverflow config to config file

---
+++ client/src/configs/coverflow.js
@@ -0,0 +1,18 @@
+export const coverFlowMedia = {
+    '@media (max-width: 361px)': {
+        width: '20px',
+        height: '300px'
+    },
+    '@media (max-width: 500px)': {
+        width: '400px',
+        height: '300px'
+    },
+    '@media (max-width: 900px)': {
+        width: '350px',
+        height: '300px'
+    },
+    '@media (min-width: 900px)': {
+        width: '900px',
+        height: '400px'
+    }
+}
