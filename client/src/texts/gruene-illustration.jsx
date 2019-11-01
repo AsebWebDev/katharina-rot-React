0564ed2a29f4d57faa5d2d2fe616cb1355d49867
André Sebastian <a.sebastian@posteo.de>
2019-11-01 20:46:02 +0100

    move text to external file

---
+++ client/src/texts/gruene-illustration.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const text_erdeHand2 = (
+    <p><strong>Klimawandel</strong>, Bienensterben, Plastikmüll, die enormen Auswirkungen von industrieller Landwirtschaft und unserer imperialen Lebensweise – die Umweltthemen unserer heutigen Gesellschaft sind mannigfaltig und erschreckend. Es ist längst an der Zeit, dass wir uns ernsthaft Gedanken darüber machen wie wir auf der Grundlage schwindender Ressourcen, steigendem Meeresspiegel und globaler Erwärmung zukünftig leben wollen und können.
+    Grundlegende Veränderungen sind dringend notwendig, aber nicht einfach umzusetzen. Umso wichtiger ist eine enge Zusammenarbeit verschiedenster Menschen und Fachbereiche – von WissenschaftlerInnen, PolitikerInnen, BewegungsakteurInnen bis hin zu HandwerkerInnen und DesignerInnen.
+    Hier kommt (auch) die Illustration ins Spiel!</p>
+)
---
+++ client/src/texts/informative-illustration.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const infoIlluMainText = (
+    <div>
+        <p>
+            Ob für Unternehmen, NGO’s, Medien oder Politik: Eine gute und ansprechende Vermittlung von Inhalten kann oft von wesentlicher Bedeutung sein.
+            Da die visuelle Wahrnehmung beim Menschen am Anfang der Informationsaufnahme steht, wird die Infografik sogar als eine der effektivsten 
+            Möglichkeiten gesehen, Sachinformationen schnell und einfach zu vermitteln. Sie strukturiert, visualisiert und vermittelt Dinge, zu denen die 
+            LeserIn vorher keinen Zugang hatte. Infografiken können sich auf das wesentliche reduzieren und Inhalte durch die <strong>Wechselwirkungen zwischen 
+            visuellem/emotionalem Eindruck und dem kommunizierenden Grafikanteil</strong> auf eine Weise veranschaulichen, die zu einem schnelleren Verständnis 
+            und zu einem gesteigerten Interesse am Thema führt. Laut einer Studie erinnern Menschen sich durchschnittlich zu etwa 10% an Gehörtes, 20% an 
+            Gelesenes und zu 80% an visuelle Eindrücke.
+        </p>
+        <p>
+            Ein inhaltlicher Schwerpunkt meiner Arbeit liegt daher auf der Erstellung von <strong>Infografiken und Illustrationen zum Zwecke 
+            der Wissensvermittlung</strong>. Ich bereite komplexe Themen gerne visuell so auf, dass sie leicht verständlich, ansprechend und schnell
+            erfassbar sind. Hierfür arbeite ich je nach Projekt sowohl mit klaren Vektorgrafiken, also auch malerisch und illustrativ. 
+            Wie Sie an den Beispielen unten sehen werden, können Informationen auf sehr unterschiedliche und vielfältige Weise visuell 
+            dargestellt werden.
+        </p>
+        <p><strong>Lassen Sie sich gerne von mir zu einer guten illustrativen Umsetzung Ihrer Themen beraten!</strong></p>
+        <p>Eine Auswahl meiner Arbeiten im Bereich der Informativen Illustration finden Sie hier:</p>
+    </div>
+    
+)
+
+    
