801d3b6d9ac78c6967e2a1a8344e94195131d054
André Sebastian <a.sebastian@posteo.de>
2019-11-02 11:20:54 +0100

    add more content

--- client/src/components/pages/GrueneIllustration.jsx
+++ client/src/components/pages/GrueneIllustration.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { MDBBtn } from "mdbreact";
 import banner from '../../media/banner-hambiwald-1024x164.jpg'
 import erdeHand from '../../media/erde-hand-icon_400px-300x300.jpg'
 import auge from '../../media/Auge_Icon_500px-1-300x300.jpg'
-import { text_erdeHand2 } from '../../texts/gruene-illustration.jsx'
+import pencil1 from '../../media/pflanzen-stift_icon-schmaler-300x300.jpg'
+import pencil2 from '../../media/Stift2_icon_600px-300x300.jpg'
+import { text_erdeHand, text_eye, text_pencil1, text_pencil2 } from '../../texts/gruene-illustration.jsx'
 import '../../styles/GrueneIllustration.scss'
 
 export default function GrueneIllustration() {
@@ -13,14 +16,22 @@ export default function GrueneIllustration() {
                 <h4 className="title">GRÜNE ILLUSTRATION</h4>
             </div>
             <div id="gruene-illu-middle">
-                <div id="gruene-illu-left">
+                <div id="gruene-illu-left" className="gruene-illu-text">
                     <img src={erdeHand} alt="Erde Hand"/>
-                    {/* <p>{text_erdeHand}</p> */}
-                    {text_erdeHand2}
-                </div>
-                <div id="gruene-illu-right">
+                    {text_erdeHand}
                     <img src={auge} alt="Erde Hand"/>
+                    {text_eye}
                 </div>
+                <div id="gruene-illu-right" className="gruene-illu-text">
+                    <img src={pencil1} alt="Erde Hand"/>
+                    {text_pencil1}
+                    <img src={pencil2} alt="Erde Hand"/>
+                    {text_pencil2}
+                    <MDBBtn outline color="success">KONTAKTIEREN SIE MICH HIER!</MDBBtn>
+                </div>
+            </div>
+            <div id="gruene-illu-bottom">
+                <div className="divider"><hr/><strong>Hier sehen Sie eine Auswahl meiner bisherigen grünen Projekte</strong><hr/></div>
             </div>
         </div>
     )
--- client/src/texts/gruene-illustration.jsx
+++ client/src/texts/gruene-illustration.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import '../styles/GrueneIllustration.scss'
 
-export const text_erdeHand2 = (
-    <p><strong>Klimawandel</strong>, Bienensterben, Plastikmüll, die enormen Auswirkungen von industrieller Landwirtschaft und unserer imperialen Lebensweise – die Umweltthemen unserer heutigen Gesellschaft sind mannigfaltig und erschreckend. Es ist längst an der Zeit, dass wir uns ernsthaft Gedanken darüber machen wie wir auf der Grundlage schwindender Ressourcen, steigendem Meeresspiegel und globaler Erwärmung zukünftig leben wollen und können.
-    Grundlegende Veränderungen sind dringend notwendig, aber nicht einfach umzusetzen. Umso wichtiger ist eine enge Zusammenarbeit verschiedenster Menschen und Fachbereiche – von WissenschaftlerInnen, PolitikerInnen, BewegungsakteurInnen bis hin zu HandwerkerInnen und DesignerInnen.
-    Hier kommt (auch) die Illustration ins Spiel!</p>
+export const text_erdeHand = (
+    <div>
+        <p><strong>Klimawandel</strong>, Bienensterben, Plastikmüll, die enormen Auswirkungen von industrieller Landwirtschaft und unserer imperialen 
+            Lebensweise – die Umweltthemen unserer heutigen Gesellschaft sind mannigfaltig und erschreckend. Es ist längst an der Zeit, dass wir uns 
+            ernsthaft Gedanken darüber machen wie wir auf der Grundlage schwindender Ressourcen, steigendem Meeresspiegel und globaler Erwärmung zukünftig 
+            leben wollen und können. Grundlegende Veränderungen sind dringend notwendig, aber nicht einfach umzusetzen. Umso wichtiger ist eine enge 
+            Zusammenarbeit verschiedenster Menschen und Fachbereiche – von WissenschaftlerInnen, PolitikerInnen, BewegungsakteurInnen bis hin zu 
+            HandwerkerInnen und DesignerInnen.
+            <br/>
+            <strong className="green-font">Hier kommt (auch) die Illustration ins Spiel!</strong>
+        </p>
+    </div>
+    
+)
+
+export const text_eye = (
+    <div>
+        <p> <strong>Eine gute visuelle Aufbereitung</strong> kann wesentlich sein für das Vermitteln von wichtigen Inhalten: Illustration kann erklären und vermitteln, 
+            Interesse wecken, Gefühle erzeugen, Zusammenhänge verdeutlichen. Sie kann Wissen breiter und leichter zur Verfügung stellen und damit helfen, 
+            notwendigen Wandel anzustoßen. Durch visuelle Metaphern kann sie Zahlen und Daten für uns einprägsam machen. Sie ist in der Lage Unsichtbares 
+            sichtbar zu machen und neue Ideen, abstrakte Konzepte, sowie komplexe Sachverhalte klar, verständlich und nachhaltig zu visualisieren.
+        </p>
+    </div>
+)
+
+export const text_pencil1= (
+    <div>
+        <p> <strong>Als Illustratorin</strong> ist es mir ein Anliegen und eine Verantwortung, mein Können bewusst als Mittel einzusetzen um 
+        eine gerechtere und grünere Welt zu schaffen. Durch meine mittlerweile über 10-jährige Erfahrung als Aktivistin in sozialen Bewegungen 
+        und durch eigene intensive Auseinandersetzung mit diversen Themen, bringe ich abgesehen von meinen handwerklichen und gestalterischen 
+        Fähigkeiten eigenes Know-How und viel Engagement in die Arbeit in grünen Projekten mit. Ich arbeite bevorzugt für Unternehmen, 
+        Organisationen und Projekte, deren Ziel es ist auf Missstände hinzuweisen, neue Perspektiven zu schaffen oder nachhaltige Konzepte zu 
+        entwickeln. Mein inhaltlicher Fokus liegt besonders auf den Themenbereichen sozialer und ökologischer Gerechtigkeit, Tierrechte, 
+        Klimaschutz und der Transformation hin zu nachhaltigen Gesellschaften
+        </p>
+    </div>
+)
+
+export const text_pencil2= (
+    <div>
+        <p> <strong>Ich gewährleiste außerdem</strong>, für die Herstellung meiner Illustrationen ausschließlich vegane Produkte zu verwenden 
+        und meine technischen Geräte mit regenerativer Energie zu laden.</p>
+        <p>Gerne unterstütze ich Sie in Ihrem Projekt und biete Vorschläge für eine illustrative Umsetzung an.
+        </p>
+    </div>
 )
