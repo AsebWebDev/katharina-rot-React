a1d5503b555c2147f3c5edb8e0df9ebd1b97b157
André Sebastian <a.sebastian@posteo.de>
2019-11-03 20:19:11 +0100

    make Collection mobile friendly

--- client/src/components/pages/Collection.jsx
+++ client/src/components/pages/Collection.jsx
@@ -1,17 +1,28 @@
 import React, {useState, useEffect} from 'react'
 import { connect } from 'react-redux';
+import $ from "jquery";
 import ParallaxLeft from '../CollectionParallaxLeft'
 import ParallaxRight from '../CollectionParallaxRight'
 import Slider from './Slider/Slider'
 import SlideModal from './Slider/SlideModal'
 import api from '../../api';
-import '../../styles/Collection.css'
+import { checkMobile, checkFullScreen } from '../../helpers'
+import '../../styles/Collection.scss'
 
 function Collection(props) {
   let [currentId] = useState(props.match.params.id)
   let [currentCollection, setCurrentCollection] = useState(null)
   let [parsedPictures, setParsedPictures] = useState([])
   let [error, setError] = useState(null)
+  let [isMobile, setIsMobile] = useState(checkMobile())
+  let [isFullScreen, setIsFullScreen] = useState(checkFullScreen())
+
+  useEffect(() => {
+    $(window).resize(function(){
+      setIsMobile(checkMobile())
+      setIsFullScreen(checkFullScreen())
+    })
+  }, [isMobile, isFullScreen])
 
   useEffect(() => {
     api.getOneCollection(currentId)
@@ -35,24 +46,37 @@ function Collection(props) {
     })
   )}
   
-  if (currentCollection)
+  if (currentCollection) {
+    let { title, description, pictures, titlePic } = currentCollection;
+    let hasPictures = pictures.length > 0
     return (
       <div className="collection">
-          <p className="title">{currentCollection.title}</p>
-          {currentCollection && currentCollection.pictures.length > 0 && <Slider heading="Example Slider" slides={parsedPictures} id={currentId} />}
-          <div id="plx-collection">
-            <div id="plx-left" className="plx">
-              <ParallaxLeft currentId={currentId}/>
-            </div>
-            <div id="plx-right" className="plx">
-              <ParallaxRight currentId={currentId}/>  
-            </div>
-          </div>
-          {props.modal && props.modal.isOpen && currentCollection.pictures && <SlideModal img={currentCollection.pictures[props.modal.currentIndex]} />}
+          <p className="title">{title}</p>
+          {!isFullScreen && hasPictures && 
+              <p>{description}</p>}
+          {hasPictures && 
+              <Slider heading="Example Slider" slides={parsedPictures} id={currentId} />}
+          {!isMobile && hasPictures 
+              && <Slider heading="Example Slider" slides={parsedPictures} id={currentId} /> 
+              &&  <div id="plx-collection">
+                    <div id="plx-left" className="plx">
+                      <ParallaxLeft currentId={currentId}/>
+                    </div>
+                    {isFullScreen && <div id="plx-right" className="plx">
+                      <ParallaxRight currentId={currentId}/>  
+                    </div>}
+                  </div>
+          }
+          {!hasPictures && 
+              <div id="no-pictures-div">
+                <img src={titlePic} alt="Titelbild"/>
+                <p>{description}</p>
+              </div>}
+          {props.modal && props.modal.isOpen && hasPictures && 
+              <SlideModal img={pictures[props.modal.currentIndex]} />}
       </div>
-
     )
-  else return (error && <p>{error}</p>)
+  } else return (error && <p>{error}</p>)
 }
 
 function mapStateToProps(reduxState){
