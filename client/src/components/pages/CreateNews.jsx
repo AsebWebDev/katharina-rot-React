4996e3c4204ea9fcc25b70be58e8d0d4a0032f72
André Sebastian <a.sebastian@posteo.de>
2019-11-07 23:40:01 +0100

    new feature: upload multiple pictures for news

--- client/src/components/pages/CreateNews.jsx
+++ client/src/components/pages/CreateNews.jsx
@@ -12,22 +12,28 @@ function CreateNews(props) {
     let {dispatch} = props;
     let [titlePic, setTitlePic] = useState('');
     let [thumbnail, setThumbnail] = useState('');
+    let [pictures, setPictures] = useState([]);
     let [currentNews, setCurrentNews] = useState({tags: []})
 
     let uploadWidget = (e) => {
         e.preventDefault();
+        let uploadedPictures = []
+        let multiple = (e.target.id === "upload-art"); // Multiple pictures for the gallery or one picture as title
         window.cloudinary.createUploadWidget({ 
             upload_preset: 'ypmn3qkk',
-            multiple: false
+            multiple
         },(error, result) => {
         console.log("TCL: uploadWidget -> result", result.info)
             if (!error && result && result.event === "success") { 
-                setTitlePic(result.info.secure_url)             // UPLOAD SINGLE PICTURE FOR THE TITLE ON UPLOAD FINISH
-                setThumbnail(result.info.thumbnail_url)
+                if (multiple) uploadedPictures.push(result.info.secure_url)   // If one of few files is uploaded, store in array to be pushed after user closes widget
+                else {
+                    setTitlePic(result.info.secure_url);             // UPLOAD SINGLE PICTURE FOR THE TITLE ON UPLOAD FINISH
+                    setThumbnail(result.info.thumbnail_url)
+                }
             }
-            // if (!error && result && result.event === "close" && multiple) {   // If user closes widget use all uploaded pictures stored while uploading
-            //     setPictures(uploadedPictures)                     // UPLOAD MULTIPLE PICTURES FOR THE GALLERY ON CLOSE
-            // }
+            if (!error && result && result.event === "close" && multiple) {   // If user closes widget use all uploaded pictures stored while uploading
+                setPictures(uploadedPictures)                     // UPLOAD MULTIPLE PICTURES FOR THE GALLERY ON CLOSE
+            }
         }).open(); 
     }
 
@@ -82,10 +88,14 @@ function CreateNews(props) {
                            <MDBInputGroup id="description" onChange={handleChange} value={currentNews.description || ''} prepend="Description" type="textarea"/>
                         </div>
                         <p id="main-menu-buttons">
+                            <p><MDBBtn onClick={uploadWidget} id="upload-art" className="cloudinary-button">Upload Art</MDBBtn></p>
                             <p><MDBBtn onClick={handleSubmit}  color="success">Submit</MDBBtn></p>
                         </p>
                     </form>
                     </CloudinaryContext>
+                    <div id="gallery-create">
+                        {pictures && pictures.map((picture, i) => <img className="mini-pic hoverable" src={picture} key ={i} alt=""/>)}
+                    </div>
                 </MDBJumbotron>
             </MDBCol>
             </MDBRow>
