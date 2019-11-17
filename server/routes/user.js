e327395a5b89a382321020491581feb86ef56726
André Sebastian <a.sebastian@posteo.de>
2019-11-17 17:20:24 +0100

    add GET route for user settings

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,16 +3,27 @@ const router = express.Router();
 const { isLoggedIn } = require('../middlewares')
 const User = require("../models/User")
 
-router.post('/:id/settings', isLoggedIn, (req, res, next) => {
-    User.findByIdAndUpdate(req.params.id, { settings: req.body })
+router.get('/:id/settings', isLoggedIn, (req, res, next) => {
+    User.findById(req.params.id)
     .then(userDoc => {
       if (!userDoc) {
         next(new Error("Could not find user settings."))
         return
       } 
+      res.json(userDoc.settings)
+    })
+    .catch(err => next(err))
+});
 
-      userDoc.password = null   // Do not send encrypted password in result
-      res.json(userDoc)
+router.post('/:id/settings', isLoggedIn, (req, res, next) => {
+    User.findByIdAndUpdate(req.params.id, { settings: req.body }, { new: true })
+    .then(userDoc => {
+      if (!userDoc) {
+        next(new Error("Could not find user settings."))
+        return
+      } 
+      console.log("TCL: userDoc.settings", userDoc.settings)
+      res.json(userDoc.settings)
     })
     .catch(err => next(err))
 });
