4f3a2c8f18a7aaba32004f6ac8fdd173dc5591a7
André Sebastian <a.sebastian@posteo.de>
2020-03-01 16:34:26 +0100

    remove unused useEffect

--- client/src/components/pages/Preferences.jsx
+++ client/src/components/pages/Preferences.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { MDBJumbotron, MDBIcon, MDBContainer, MDBRow, MDBCol, MDBListGroup, MDBListGroupItem } from "mdbreact";
 import Button from '@material-ui/core/Button';
